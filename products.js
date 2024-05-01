1f6ae5676311fdfffacb011e77a7635d6ebd34e2
Muhad M <134828945+muhadmmm@users.noreply.github.com>
2024-05-01 18:31:44 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 // products.js
 const products = [
-    { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 10, imageUrl: 'product1.jpg' },
+    { id: 1, name: 'wall frame 1', description: 'Display your memories beautifully with our sleek frame. Designed to complement any decor, it's the perfect way to showcase your favorite photos', price: 100, imageUrl: 'IMG20230425121420.jpg' },
     { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 20, imageUrl: 'product2.jpg' },
     { id: 3, name: 'Product 3', description: 'Description of Product 3', price: 30, imageUrl: 'product3.jpg' }
 ];
