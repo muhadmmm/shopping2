f74025c373854b7c8b6c5e66fd022c2a7d1e48c5
Muhad M <134828945+muhadmmm@users.noreply.github.com>
2024-05-01 18:10:32 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+// products.js
+const products = [
+    { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 10, imageUrl: 'product1.jpg' },
+    { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 20, imageUrl: 'product2.jpg' },
+    { id: 3, name: 'Product 3', description: 'Description of Product 3', price: 30, imageUrl: 'product3.jpg' }
+];
+
+module.exports = products;
