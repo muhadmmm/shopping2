f2e8b0dd30d48737ddd1da9356943ef090ec7461
Muhad M <134828945+muhadmmm@users.noreply.github.com>
2024-05-02 23:30:22 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,23 @@
 const products = [
   {
     "name": "ASL",
-    "imageUrl": "https://drive.google.com/file/d/1jJJtdRtsZh8Ieqa6K9BfoobVKtTXVLhT/view?usp=drivesdk",
+    "imageUrl": "imgs.jpg",
     "description": "Ace your English is not for kids to come to your room in the evening and i will be a little bit of a man who is your fav",
     "price": 500,
-    "phoneNumber": "9539085851",
+    "phoneNumber": "9539085751",
     "text": "One-piece"
   },
   {
     "name": "Gear5",
-    "imageUrl": "https://drive.google.com/uc?id=1jfrK-OW59ZspPIGOYPA3W3NoRgAmiAer",
+    "imageUrl": "imgs.jpg",
     "description": "Gear5 to the group for share price list of a man who was there for the late reply to this email id and password is not",
     "price": 250,
     "phoneNumber": "9539085751",
     "text": "Wanted"
   },
   {
-    "name": "Wanted",
-    "imageUrl": "https://drive.google.com/uc?id=1jK15wd55jA3gvat6j-Ka2JxJDzVZQh8s",
+    "name": "Wanted-poster luffy",
+    "imageUrl": "imgs.jpg",
     "description": "Wanted to know the value of the year of the year of experience in the evening and i am not sure about the world cup",
     "price": 550,
     "phoneNumber": "9539085751",
@@ -25,7 +25,7 @@ const products = [
   },
   {
     "name": "Onepiece",
-    "imageUrl": "https://drive.google.com/uc?id=1jHGkILivWw9eHvBGwYL9Jn_qSivskRhI",
+    "imageUrl": "imgs.jpg",
     "description": "Onepiece luffy gear 5 designs for men and boys and girls are you doing well in the evening and i am not sure about the",
     "price": 600,
     "phoneNumber": "9539085751",
@@ -33,11 +33,10 @@ const products = [
   },
   {
     "name": "Sun god nika",
-    "imageUrl": "https://drive.google.com/uc?id=1jN7jcjDxsnyZR9UL34ytF4mLf0wSFkGB",
+    "imageUrl": "imgs.jpg",
     "description": "Sun god nika onepiece luffy wanted to know the value of the year of the year of the year of the year of the year of the",
     "price": 360,
     "phoneNumber": "9539085751",
     "text": "Sungodnika"
   }
 ];
-
